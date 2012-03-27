3f0e971a2556986261a5a74fff3d549c0414e90e
Alex Kaiser <atkaiser@berkeley.edu>
2012-03-26 20:07:50 -0700

    Added department_long to the course getter

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,6 @@
 class Course < ActiveRecord::Base
-	has_many :requirements
-	has_many :books, :through => :requirements
+  has_many :requirements
+  has_many :books, :through => :requirements
   #Validates the same course isn't added twice
   validates :term, :uniqueness => { :scope => [ :number, :name, :department_short, :department_long, :teacher, :section, :year ] }
 
@@ -9,14 +9,18 @@ class Course < ActiveRecord::Base
     agent = Mechanize.new
     courses_page = agent.get('http://osoc.berkeley.edu/OSOC/osoc?p_term=' + term_acronym + '&p_list_all=Y')
     list_of_course_objects = courses_page.root.css('body td td label')
+    department_long_couter = 3
     list_of_course_objects.each_slice(3) do |course_tuple|
+      if course[:department_short] != course_tuple[0].content
+        department_long_counter += 1
+      end
       course = {}
       course[:department_short] = course_tuple[0].content
       course[:number] = course_tuple[1].content[3..-1]
       course[:name] = course_tuple[2].content
       course[:term] = term
       course[:year] = year
-      #add department_long
+      course[:department_long] = courses_page.root.css("body td td font")[department_long_counter].content
       get_teacher(courses_page, course).each do |teacher|
         #add section_info
         course[:teacher] = teacher
@@ -46,9 +50,9 @@ class Course < ActiveRecord::Base
     end
     return "Bob"
   end
-  
+
   #returns the required books and unrequired books of the course with :id == id as an array of 2 arrays eg [[list of required books][list of unrequired books]]
   def find_required_and_unrequired_books(id)
-    
+
   end
 end
