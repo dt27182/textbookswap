e97309dc6810100ddadd5ce8231eec5f3c109a0e
Alex Kaiser <atkaiser@berkeley.edu>
2012-04-05 11:36:43 -0700

    updated the get_courses method

--- app/models/course.rb
+++ app/models/course.rb
@@ -21,9 +21,9 @@ class Course < ActiveRecord::Base
       course[:term] = term
       course[:year] = year
       course[:department_long] = courses_page.root.css("body td td font")[department_long_counter].content
-      get_teacher(courses_page, course).each do |teacher|
-        #add section_info
-        course[:teacher] = teacher
+      get_teacher(courses_page, course).each do |teacher_and_section|
+        course[:section] = teacher_and_section[1]
+        course[:teacher] = teacher_and_section[0]
         c = Course.new(course)
         c.save
       end
@@ -43,12 +43,17 @@ class Course < ActiveRecord::Base
     form.p_title = course[:name]
     detailed_course_page = agent.submit(form)
     page_elements = detailed_course_page.root.css('body table tr td')
-    page_elements.each_conts(2) do |two_elements|
-      if two_elements[0].content =~ /Instructor/
-        return two_elements[1].content
+    teacher_pairs = []
+    page_elements.each_conts(8) do |elements|
+      if elements[0].content =~ /Course/
+        if elements[1] =~ /(LEC)|(SEM)/
+          teacher = elements[7]
+          section = elements[1].gsub(/\s+/, "")[-6..-4]
+          teacher_pairs = [teacher, section]
+        end
       end
     end
-    return "Bob"
+    return teacher_pairs
   end
 
   #returns the required books and unrequired books of the course with :id == id as an array of 2 arrays eg [[list of required books][list of unrequired books]]
