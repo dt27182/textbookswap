aee1f2f8bed25217937cdb30d97710b4c3805d7b
Danny Tang <dt27182@berkeley.edu>
2012-04-07 23:24:40 -0700

    fixed java script for course selection page drop down

--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -1,14 +1,16 @@
-(function() {
-	$("#course_department").change(function(){
-		$.getJSON(
-			"/course/find_course_numbers", 
-			{department : $("#course_department").val()},
-			function(json){
-				$("#course_number").empty();
-				$.each(json, function(val)){
-					$("#course_number").append('<option value="' + val + '">' + val + '</option>');
-				};	
+$(document).ready(
+	function(){
+		$("#course_department").change(
+			function(){
+				$.getJSON("/course/find_course_numbers", {department : $("#course_department").val()}, function(json){
+					$("#course_number").empty();
+					$.each(json, function(val){
+						$("#course_number").append('<option value="' + val + '">' + val + '</option>');
+					});
+				});
+				$("#main").append("<h1>javascript suceeded</h1>");
 			}
 		);
-	};
-}).call(this);
+	}
+);
+					
