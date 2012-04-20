1d20cb059fe63875f2b36c74ce4353c7002d3839
Danny Tang <dt27182@berkeley.edu>
2012-04-20 15:57:16 -0700

    fixed validation in books model

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,6 @@ class Book < ActiveRecord::Base
   has_many :requirements
   has_many :courses, :through => :requirements
   validates :title, :author, :edition, :isbn, :presence => true
-  #validates :edition, :isbn, {:scope => [:title, :isbn]}
+  validates :edition, :uniqueness => {:scope => [:title, :author]}
   #validates :isbn, :isbn_format => true
 end
