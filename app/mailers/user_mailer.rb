1c061e2de6522fa9493ff19547afef0772525387
Alex Kaiser <atkaiser@berkeley.edu>
2012-04-20 16:42:32 -0700

    Email working

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,10 @@
 class UserMailer < ActionMailer::Base
   default :from => "from@example.com"
 
-  def self.test_email(address)
-    mail(:to => address, :subject => "Welcome")
+  def send_seller_buyer_email(to_address, buyer_email, body)
+    @body = body
+    @buyer_email = buyer_email
+    mail(:to => to_address, :subject => "Someone wants to buy your book", :from => "TextbookSwap")
   end
 
 end
