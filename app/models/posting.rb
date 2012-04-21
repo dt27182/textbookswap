c44aa929a1549bcb0b48c04d3f177f8558f78852
Danny Tang <dt27182@berkeley.edu>
2012-04-21 16:06:57 -0700

    more fixes for email

--- app/models/posting.rb
+++ app/models/posting.rb
@@ -4,6 +4,6 @@ class Posting < ActiveRecord::Base
 	validates :seller_email, :email => {:mx => true, :message => I18n.t('validations.errors.models.user.invalid_email')}
 	
   def send_seller_buyer_info(buyer_email, body)
-    UserMailer.send_seller_buyer_email(self.seller_email, buyer_email, body, Book.find_by_id(self.book_id)).deliver
+    UserMailer.send_seller_buyer_email(self.seller_email, buyer_email, body, Book.find_by_id(self.book_id).title).deliver
 	end
 end
