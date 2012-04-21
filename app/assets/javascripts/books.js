42e018e7a7ec84fe6985eb29092a62ab0dd7b67e
Danny Tang <dt27182@berkeley.edu>
2012-04-20 20:58:38 -0700

    added flash messages and javascript client side validation

---
+++ app/assets/javascripts/books.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    $("#create_book_form").validate();
+  });
---
+++ app/assets/javascripts/postings.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    $("#buyer_info_form").validate();
+    $("#create_posting_form").validate();
+  });
