4e41c57a3760d30d2262ae97a59f773154c6b516
Danny Tang <dt27182@berkeley.edu>
2012-04-20 17:23:19 -0700

    fixed typo in posting model

--- app/models/posting.rb
+++ app/models/posting.rb
@@ -6,5 +6,5 @@ class Posting < ActiveRecord::Base
 	
   def send_seller_buyer_info(buyer_email, body)
     UserMailer.send_seller_buyer_email(self.seller_email, buyer_email, body).deliver
-
+	end
 end
