f8ecca82490b8a91bd9b3c5f2bdabdae615eeb4b
Danny Tang <dt27182@berkeley.edu>
2012-04-20 17:16:59 -0700

    changed isbn column in books to string

--- app/models/posting.rb
+++ app/models/posting.rb
@@ -1,8 +1,9 @@
 class Posting < ActiveRecord::Base
 
   belongs_to :books
-  validates :seller_email, :price, :condition, :comments, :presence => true
-
+  validates :seller_email, :price, :condition, :presence => true
+	validates :seller_email, :email => {:mx => true, :message => I18n.t('validations.errors.models.user.invalid_email')}
+	
   def send_seller_buyer_info(body)
     UserMailer.test_email("akaiser2@mac.com").deliver
   end
