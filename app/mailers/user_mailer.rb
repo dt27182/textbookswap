fe021d9f2d0ae1b889f0fbff99320abab0323569
Alex Kaiser <atkaiser@berkeley.edu>
2012-04-21 10:23:30 -0700

    Added some specs for the UserMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ActionMailer::Base
   default :from => "from@example.com"
 
-  def send_seller_buyer_email(to_address, buyer_email, body)
-    @body = body
+  def send_seller_buyer_email(to_address, buyer_email, comments)
+    @comments = comments
     @buyer_email = buyer_email
     mail(:to => to_address, :subject => "Someone wants to buy your book", :from => "TextbookSwap")
   end
