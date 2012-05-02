04ed6198b3c0680d9725b8bc771f700558fbcaa1
Alex Kaiser <atkaiser@berkeley.edu>
2012-05-01 20:08:20 -0700

    added redirect to logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,6 +18,7 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     flash[:notice] = "You have successfully logged out"
+    redirect_to index_path()
   end
     
   
